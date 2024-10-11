833dfc58d697850a9f0a7d2ddfa4bf9ef0b6218b
Cedrick C. Alegsao <cedrickcuencaalegsao@gmail.com>
2024-10-11 18:34:06 +0800

    Adding API on Web(next.js) and Mobile(Flutter)

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http; // Import the http package
+import 'dart:convert'; // Import for JSON decoding
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +11,65 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String data = '';
+  bool isLoading = true;
+  String error = '';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      final response =
+          await http.get(Uri.parse('http://127.0.0.1:8000/api/hello-world'));
+
+      if (response.statusCode == 200) {
+        setState(() {
+          data = json.decode(response.body); // Decode JSON and update the data
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          error = 'Error: ${response.statusCode}';
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        error = 'An error occurred';
+        isLoading = false;
+      });
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('API Fetch Example'),
+      ),
+      body: Center(
+        child: isLoading
+            ? CircularProgressIndicator()
+            : error.isNotEmpty
+                ? Text(error)
+                : Text('Data: $data'),
       ),
     );
   }
---
+++ web/src/app/Auth/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function AuthPage() {
+  const [data, setData] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://127.0.0.1:8000/api/hello-world"
+        );
+        setData(response.data);
+      } catch (err) {
+        setError("error");
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>{data}</h1>
+      <p>{error}</p>
+    </div>
+  );
+}
