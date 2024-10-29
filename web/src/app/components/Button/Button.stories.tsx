7d4928e5c42e2c68d20af34f6ad0733956ef8649
Cedrick C. Alegsao <cedrickcuencaalegsao@gmail.com>
2024-10-29 22:47:47 +0800

    Modifying web and  adding pages

---
+++ web/src/app/components/Button/Button.stories.tsx
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+export default {
+  title: "Button",
+  component: Button,
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: "Click Me",
+};
---
+++ web/src/app/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+const Button = ({ label }) => {
+        return (
+          <button className="btn bg-blue-500 text-white rounded-full hover:bg-blue-700 active:bg-blue-800">
+  Styled Button
+</button>
+        );
+      };
+      
+      export default Button;
+      
